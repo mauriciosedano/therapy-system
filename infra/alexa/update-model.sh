39624765cfe829851aee49df12cd97f5aabf9cf8
ramaka <mauriciosedano@gmail.com>
2024-11-26 06:21:20 -0500

    refactor: limpia y simplifica estructura del proyecto
    - Elimina archivos duplicados y backups
    - Consolida infraestructura
    - Reorganiza estructura de src
    - Simplifica organización de tests

--- infra/alexa/update-model.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ask smapi set-interaction-model \
-  --skill-id amzn1.ask.skill.028ef8b0-b7cd-45e7-bc9c-08a4a213921e \
-  --locale es-ES \
-  --interaction-model file:/Users/mase/Documents/desarrollo_APP-ALEXA-WS/BETA/therapy-system/alexa/model/interaction_model.json
-
-
